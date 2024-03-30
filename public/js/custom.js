714f23a806db089f6a436b3f35b1f31b21b34993
YangRL <yangrafaels@gmail.com>
2024-03-30 13:30:58 -0300

    Ajuste scroll mobile

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,18 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        section.addEventListener('wheel', (e) => {
-            const isSection1 = section.classList.contains('section1');
-            const isSection2 = section.classList.contains('section2');
-            const deltaY = e.deltaY;
-            const scrollable = deltaY > 0 && !section.nextElementSibling || deltaY < 0 && !section.previousElementSibling;
-            if (isSection1) {
-                $('.fade-in-R').addClass('show');
-                $('.fade-in-L').addClass('show');
-            } else if (isSection2) {
-                $('.fade-in-Down').addClass('show');
-            }
-            if (scrollable) e.preventDefault();
+const isMobile = window.innerWidth <= 768;
+if (isMobile == true) {
+    $('.fade-in-Down').addClass('show');
+    $('.fade-in-R').addClass('show');
+    $('.fade-in-L').addClass('show');
+} else {
+    document.addEventListener('DOMContentLoaded', () => {
+        const sections = document.querySelectorAll('.section');
+        sections.forEach(section => {
+            section.addEventListener('wheel', (e) => {
+                const deltaY = e.deltaY;
+                const scrollable = deltaY > 0 && !section.nextElementSibling || deltaY < 0 && !section.previousElementSibling;
+                if (deltaY > 0 && !scrollable) {
+                    if (section.nextElementSibling.classList.contains('section2')) {
+                        $('.fade-in-R').addClass('show');
+                        $('.fade-in-L').addClass('show');
+                    }
+                    if (section.nextElementSibling.classList.contains('section3')) $('.fade-in-Down').addClass('show');
+                }
+                if (scrollable == true && deltaY > 0) {
+                    $('.fade-in-Down').addClass('show');
+                    $('.fade-in-R').addClass('show');
+                    $('.fade-in-L').addClass('show');
+                }
+                if (scrollable) e.preventDefault();
+            });
         });
     });
-});
+}
