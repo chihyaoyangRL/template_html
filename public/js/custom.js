112311b7447e1300c3cb2e6e8b7703bfdbc269be
YangRL <yangrafaels@gmail.com>
2024-03-30 13:03:39 -0300

    Add scroll animation

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.section');
     sections.forEach(section => {
         section.addEventListener('wheel', (e) => {
+            const isSection1 = section.classList.contains('section1');
+            const isSection2 = section.classList.contains('section2');
             const deltaY = e.deltaY;
             const scrollable = deltaY > 0 && !section.nextElementSibling || deltaY < 0 && !section.previousElementSibling;
-            if (scrollable) {
-                e.preventDefault();
+            if (isSection1) {
+                $('.fade-in-R').addClass('show');
+                $('.fade-in-L').addClass('show');
+            } else if (isSection2) {
+                $('.fade-in-Down').addClass('show');
             }
+            if (scrollable) e.preventDefault();
         });
     });
 });
